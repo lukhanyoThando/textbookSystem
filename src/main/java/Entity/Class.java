f6d484e57667b4b5067c37980efceb117fe3f12f
lukhanyoThando <60959721+lukhanyoThando@users.noreply.github.com>
2020-06-28 12:13:01 +0200

    Update Class.java

--- src/main/java/Entity/Class.java
+++ src/main/java/Entity/Class.java
@@ -15,7 +15,7 @@ public class Class {
     @Override
     public String toString() {
         return "Class{" +
-                "studCourse=" + studCourse +
+                "studCourse="+""+ studCourse +
                 '}';
     }
 
